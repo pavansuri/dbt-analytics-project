52797375abaa9ac4797232ee71d36fd6aaaf9cac
pavansuri <79553169+pavansuri@users.noreply.github.com>
2026-04-03 11:23:30 -0400

    Create stg_products.sql for staging product data

---
+++ models/staging/models/staging/models/staging/stg_products.sql
@@ -0,0 +1,16 @@
+{{ config(schema='staging') }}
+
+with source as (
+    select * from {{ source('raw', 'products') }}
+),
+
+renamed as (
+    select
+        product_id,
+        product_name,
+        category,
+        price
+    from source
+)
+
+select * from renamed
